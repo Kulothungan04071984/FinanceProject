85cfc97c6c0cf8f8332234ca746a3cde77a215b9
kulothungan <kulothungan.k@syrmasgs.com>
2024-08-31 15:32:04 +0530

    Add project files.

---
+++ Finance/Menu.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Finance
+{
+    partial class Menu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            masterToolStripMenuItem = new ToolStripMenuItem();
+            aCCOUNTToolStripMenuItem = new ToolStripMenuItem();
+            bANKToolStripMenuItem = new ToolStripMenuItem();
+            iNTERESTMASTERToolStripMenuItem = new ToolStripMenuItem();
+            iNSURANCECOMPANYMASTERToolStripMenuItem = new ToolStripMenuItem();
+            lADGERGROUPMASTERToolStripMenuItem = new ToolStripMenuItem();
+            lEDGERMASTERToolStripMenuItem = new ToolStripMenuItem();
+            eMPLOYEEMASTERToolStripMenuItem = new ToolStripMenuItem();
+            eXPENSESMASTERToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            aCCOUNTRECEPTSPAYMENTToolStripMenuItem = new ToolStripMenuItem();
+            rECEIPTSANDCHEQUEToolStripMenuItem = new ToolStripMenuItem();
+            eMPLOYEEToolStripMenuItem = new ToolStripMenuItem();
+            eXPENSESToolStripMenuItem = new ToolStripMenuItem();
+            interestLoanToolStripMenuItem = new ToolStripMenuItem();
+            cUSTOMERMASTERToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            rECEPTToolStripMenuItem = new ToolStripMenuItem();
+            dASHBOARDToolStripMenuItem = new ToolStripMenuItem();
+            rEPORTSToolStripMenuItem = new ToolStripMenuItem();
+            uTILITESToolStripMenuItem = new ToolStripMenuItem();
+            dOCUMENTENTRYToolStripMenuItem = new ToolStripMenuItem();
+            sCHDULELISTToolStripMenuItem = new ToolStripMenuItem();
+            fORECLOSEToolStripMenuItem = new ToolStripMenuItem();
+            nOCToolStripMenuItem = new ToolStripMenuItem();
+            dOCUMENTUPLOADToolStripMenuItem = new ToolStripMenuItem();
+            dOCUMENTVIEWToolStripMenuItem = new ToolStripMenuItem();
+            eMPLOYEEToolStripMenuItem1 = new ToolStripMenuItem();
+            eXPENSESToolStripMenuItem1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.CornflowerBlue;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { masterToolStripMenuItem, toolStripMenuItem1, interestLoanToolStripMenuItem, dASHBOARDToolStripMenuItem, rEPORTSToolStripMenuItem, uTILITESToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // masterToolStripMenuItem
+            // 
+            masterToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aCCOUNTToolStripMenuItem, bANKToolStripMenuItem, iNTERESTMASTERToolStripMenuItem, iNSURANCECOMPANYMASTERToolStripMenuItem, lADGERGROUPMASTERToolStripMenuItem, lEDGERMASTERToolStripMenuItem, eMPLOYEEMASTERToolStripMenuItem, eXPENSESMASTERToolStripMenuItem });
+            masterToolStripMenuItem.ForeColor = Color.Black;
+            masterToolStripMenuItem.Name = "masterToolStripMenuItem";
+            masterToolStripMenuItem.Size = new Size(63, 20);
+            masterToolStripMenuItem.Text = "MASTER";
+            // 
+            // aCCOUNTToolStripMenuItem
+            // 
+            aCCOUNTToolStripMenuItem.Name = "aCCOUNTToolStripMenuItem";
+            aCCOUNTToolStripMenuItem.Size = new Size(246, 22);
+            aCCOUNTToolStripMenuItem.Text = "ACCOUNT MASTER";
+            // 
+            // bANKToolStripMenuItem
+            // 
+            bANKToolStripMenuItem.Name = "bANKToolStripMenuItem";
+            bANKToolStripMenuItem.Size = new Size(246, 22);
+            bANKToolStripMenuItem.Text = "BANK";
+            // 
+            // iNTERESTMASTERToolStripMenuItem
+            // 
+            iNTERESTMASTERToolStripMenuItem.Name = "iNTERESTMASTERToolStripMenuItem";
+            iNTERESTMASTERToolStripMenuItem.Size = new Size(246, 22);
+            iNTERESTMASTERToolStripMenuItem.Text = "INTEREST MASTER";
+            // 
+            // iNSURANCECOMPANYMASTERToolStripMenuItem
+            // 
+            iNSURANCECOMPANYMASTERToolStripMenuItem.Name = "iNSURANCECOMPANYMASTERToolStripMenuItem";
+            iNSURANCECOMPANYMASTERToolStripMenuItem.Size = new Size(246, 22);
+            iNSURANCECOMPANYMASTERToolStripMenuItem.Text = "INSURANCE COMPANY MASTER";
+            // 
+            // lADGERGROUPMASTERToolStripMenuItem
+            // 
+            lADGERGROUPMASTERToolStripMenuItem.Name = "lADGERGROUPMASTERToolStripMenuItem";
+            lADGERGROUPMASTERToolStripMenuItem.Size = new Size(246, 22);
+            lADGERGROUPMASTERToolStripMenuItem.Text = "LADGER GROUP MASTER";
+            // 
+            // lEDGERMASTERToolStripMenuItem
+            // 
+            lEDGERMASTERToolStripMenuItem.Name = "lEDGERMASTERToolStripMenuItem";
+            lEDGERMASTERToolStripMenuItem.Size = new Size(246, 22);
+            lEDGERMASTERToolStripMenuItem.Text = "LEDGER MASTER";
+            // 
+            // eMPLOYEEMASTERToolStripMenuItem
+            // 
+            eMPLOYEEMASTERToolStripMenuItem.Name = "eMPLOYEEMASTERToolStripMenuItem";
+            eMPLOYEEMASTERToolStripMenuItem.Size = new Size(246, 22);
+            eMPLOYEEMASTERToolStripMenuItem.Text = "EMPLOYEE MASTER";
+            // 
+            // eXPENSESMASTERToolStripMenuItem
+            // 
+            eXPENSESMASTERToolStripMenuItem.Name = "eXPENSESMASTERToolStripMenuItem";
+            eXPENSESMASTERToolStripMenuItem.Size = new Size(246, 22);
+            eXPENSESMASTERToolStripMenuItem.Text = "EXPENSES MASTER";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { aCCOUNTRECEPTSPAYMENTToolStripMenuItem, rECEIPTSANDCHEQUEToolStripMenuItem, eMPLOYEEToolStripMenuItem, eXPENSESToolStripMenuItem, eMPLOYEEToolStripMenuItem1, eXPENSESToolStripMenuItem1 });
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(98, 20);
+            toolStripMenuItem1.Text = "TRANSACTION";
+            // 
+            // aCCOUNTRECEPTSPAYMENTToolStripMenuItem
+            // 
+            aCCOUNTRECEPTSPAYMENTToolStripMenuItem.Name = "aCCOUNTRECEPTSPAYMENTToolStripMenuItem";
+            aCCOUNTRECEPTSPAYMENTToolStripMenuItem.Size = new Size(240, 22);
+            aCCOUNTRECEPTSPAYMENTToolStripMenuItem.Text = "ACCOUNT RECEIPTS & PAYMENT";
+            // 
+            // rECEIPTSANDCHEQUEToolStripMenuItem
+            // 
+            rECEIPTSANDCHEQUEToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { dOCUMENTENTRYToolStripMenuItem, sCHDULELISTToolStripMenuItem, fORECLOSEToolStripMenuItem, nOCToolStripMenuItem, dOCUMENTUPLOADToolStripMenuItem, dOCUMENTVIEWToolStripMenuItem });
+            rECEIPTSANDCHEQUEToolStripMenuItem.Name = "rECEIPTSANDCHEQUEToolStripMenuItem";
+            rECEIPTSANDCHEQUEToolStripMenuItem.Size = new Size(240, 22);
+            rECEIPTSANDCHEQUEToolStripMenuItem.Text = "LOAN";
+            // 
+            // eMPLOYEEToolStripMenuItem
+            // 
+            eMPLOYEEToolStripMenuItem.Name = "eMPLOYEEToolStripMenuItem";
+            eMPLOYEEToolStripMenuItem.Size = new Size(240, 22);
+            eMPLOYEEToolStripMenuItem.Text = "RECEIPTS";
+            // 
+            // eXPENSESToolStripMenuItem
+            // 
+            eXPENSESToolStripMenuItem.Name = "eXPENSESToolStripMenuItem";
+            eXPENSESToolStripMenuItem.Size = new Size(240, 22);
+            eXPENSESToolStripMenuItem.Text = "DEPOSIT AND CHEQUE";
+            // 
+            // interestLoanToolStripMenuItem
+            // 
+            interestLoanToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cUSTOMERMASTERToolStripMenuItem, toolStripMenuItem2, rECEPTToolStripMenuItem });
+            interestLoanToolStripMenuItem.Name = "interestLoanToolStripMenuItem";
+            interestLoanToolStripMenuItem.Size = new Size(185, 20);
+            interestLoanToolStripMenuItem.Text = "INTEREST LOAN TRANSACTION";
+            // 
+            // cUSTOMERMASTERToolStripMenuItem
+            // 
+            cUSTOMERMASTERToolStripMenuItem.Name = "cUSTOMERMASTERToolStripMenuItem";
+            cUSTOMERMASTERToolStripMenuItem.Size = new Size(188, 22);
+            cUSTOMERMASTERToolStripMenuItem.Text = "CUSTOMER MASTER";
+            cUSTOMERMASTERToolStripMenuItem.Click += cUSTOMERMASTERToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(188, 22);
+            toolStripMenuItem2.Text = "LOAN TRANSACTION";
+            // 
+            // rECEPTToolStripMenuItem
+            // 
+            rECEPTToolStripMenuItem.Name = "rECEPTToolStripMenuItem";
+            rECEPTToolStripMenuItem.Size = new Size(188, 22);
+            rECEPTToolStripMenuItem.Text = "RECEPT";
+            // 
+            // dASHBOARDToolStripMenuItem
+            // 
+            dASHBOARDToolStripMenuItem.Name = "dASHBOARDToolStripMenuItem";
+            dASHBOARDToolStripMenuItem.Size = new Size(89, 20);
+            dASHBOARDToolStripMenuItem.Text = "DASHBOARD";
+            // 
+            // rEPORTSToolStripMenuItem
+            // 
+            rEPORTSToolStripMenuItem.Name = "rEPORTSToolStripMenuItem";
+            rEPORTSToolStripMenuItem.Size = new Size(66, 20);
+            rEPORTSToolStripMenuItem.Text = "REPORTS";
+            // 
+            // uTILITESToolStripMenuItem
+            // 
+            uTILITESToolStripMenuItem.Name = "uTILITESToolStripMenuItem";
+            uTILITESToolStripMenuItem.Size = new Size(63, 20);
+            uTILITESToolStripMenuItem.Text = "UTILITES";
+            // 
+            // dOCUMENTENTRYToolStripMenuItem
+            // 
+            dOCUMENTENTRYToolStripMenuItem.Name = "dOCUMENTENTRYToolStripMenuItem";
+            dOCUMENTENTRYToolStripMenuItem.Size = new Size(188, 22);
+            dOCUMENTENTRYToolStripMenuItem.Text = "DOCUMENT ENTRY";
+            // 
+            // sCHDULELISTToolStripMenuItem
+            // 
+            sCHDULELISTToolStripMenuItem.Name = "sCHDULELISTToolStripMenuItem";
+            sCHDULELISTToolStripMenuItem.Size = new Size(188, 22);
+            sCHDULELISTToolStripMenuItem.Text = "SCHDULELIST";
+            // 
+            // fORECLOSEToolStripMenuItem
+            // 
+            fORECLOSEToolStripMenuItem.Name = "fORECLOSEToolStripMenuItem";
+            fORECLOSEToolStripMenuItem.Size = new Size(188, 22);
+            fORECLOSEToolStripMenuItem.Text = "FORE CLOSE";
+            // 
+            // nOCToolStripMenuItem
+            // 
+            nOCToolStripMenuItem.Name = "nOCToolStripMenuItem";
+            nOCToolStripMenuItem.Size = new Size(188, 22);
+            nOCToolStripMenuItem.Text = "N.O.C";
+            // 
+            // dOCUMENTUPLOADToolStripMenuItem
+            // 
+            dOCUMENTUPLOADToolStripMenuItem.Name = "dOCUMENTUPLOADToolStripMenuItem";
+            dOCUMENTUPLOADToolStripMenuItem.Size = new Size(188, 22);
+            dOCUMENTUPLOADToolStripMenuItem.Text = "DOCUMENT UPLOAD";
+            // 
+            // dOCUMENTVIEWToolStripMenuItem
+            // 
+            dOCUMENTVIEWToolStripMenuItem.Name = "dOCUMENTVIEWToolStripMenuItem";
+            dOCUMENTVIEWToolStripMenuItem.Size = new Size(188, 22);
+            dOCUMENTVIEWToolStripMenuItem.Text = "DOCUMENT VIEW";
+            // 
+            // eMPLOYEEToolStripMenuItem1
+            // 
+            eMPLOYEEToolStripMenuItem1.Name = "eMPLOYEEToolStripMenuItem1";
+            eMPLOYEEToolStripMenuItem1.Size = new Size(240, 22);
+            eMPLOYEEToolStripMenuItem1.Text = "EMPLOYEE";
+            // 
+            // eXPENSESToolStripMenuItem1
+            // 
+            eXPENSESToolStripMenuItem1.Name = "eXPENSESToolStripMenuItem1";
+            eXPENSESToolStripMenuItem1.Size = new Size(240, 22);
+            eXPENSESToolStripMenuItem1.Text = "EXPENSES";
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            ForeColor = Color.SaddleBrown;
+            MainMenuStrip = menuStrip1;
+            Name = "Menu";
+            Text = "Finance";
+            KeyDown += Menu_KeyDown;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem masterToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem interestLoanToolStripMenuItem;
+        private ToolStripMenuItem dASHBOARDToolStripMenuItem;
+        private ToolStripMenuItem rEPORTSToolStripMenuItem;
+        private ToolStripMenuItem uTILITESToolStripMenuItem;
+        private ToolStripMenuItem cUSTOMERMASTERToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem rECEPTToolStripMenuItem;
+        private ToolStripMenuItem aCCOUNTRECEPTSPAYMENTToolStripMenuItem;
+        private ToolStripMenuItem rECEIPTSANDCHEQUEToolStripMenuItem;
+        private ToolStripMenuItem eMPLOYEEToolStripMenuItem;
+        private ToolStripMenuItem eXPENSESToolStripMenuItem;
+        private ToolStripMenuItem aCCOUNTToolStripMenuItem;
+        private ToolStripMenuItem bANKToolStripMenuItem;
+        private ToolStripMenuItem iNTERESTMASTERToolStripMenuItem;
+        private ToolStripMenuItem iNSURANCECOMPANYMASTERToolStripMenuItem;
+        private ToolStripMenuItem lADGERGROUPMASTERToolStripMenuItem;
+        private ToolStripMenuItem lEDGERMASTERToolStripMenuItem;
+        private ToolStripMenuItem eMPLOYEEMASTERToolStripMenuItem;
+        private ToolStripMenuItem eXPENSESMASTERToolStripMenuItem;
+        private ToolStripMenuItem dOCUMENTENTRYToolStripMenuItem;
+        private ToolStripMenuItem sCHDULELISTToolStripMenuItem;
+        private ToolStripMenuItem fORECLOSEToolStripMenuItem;
+        private ToolStripMenuItem nOCToolStripMenuItem;
+        private ToolStripMenuItem dOCUMENTUPLOADToolStripMenuItem;
+        private ToolStripMenuItem dOCUMENTVIEWToolStripMenuItem;
+        private ToolStripMenuItem eMPLOYEEToolStripMenuItem1;
+        private ToolStripMenuItem eXPENSESToolStripMenuItem1;
+    }
+}
---
+++ Finance/Menu.cs
@@ -0,0 +1,83 @@
+namespace Finance
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            this.KeyDown += Menu_KeyDown;
+            this.KeyPreview = true;
+
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+
+            MenuStrip menuStrip1 = new MenuStrip();
+            this.Controls.Add(menuStrip1);
+
+            // Create Menu Items
+            //ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
+            //ToolStripMenuItem editMenu = new ToolStripMenuItem("Edit");
+
+            //menuStrip1.Items.Add(fileMenu);
+            //menuStrip1.Items.Add(editMenu);
+
+            // Apply custom renderer
+            menuStrip1.Renderer = new CustomMenuRenderer();
+
+            // Set custom font
+            menuStrip1.Font = new Font("Segoe UI", 10, FontStyle.Regular);
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            this.TopMost = true;
+
+
+
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ClassStyle |= 0x200; // CS_NOCLOSE
+                return cp;
+            }
+        }
+
+        public class CustomMenuRenderer : ToolStripProfessionalRenderer
+        {
+            protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e)
+            {
+                if (e.Item.Selected)
+                {
+                    e.Graphics.FillRectangle(Brushes.LightBlue, e.Item.Bounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(Brushes.Silver, e.Item.Bounds);
+                }
+            }
+
+            protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+            {
+                // Do nothing to remove the border
+            }
+        }
+
+        private void cUSTOMERMASTERToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CUSTOMER TRANSACTION");
+        }
+
+        private void Menu_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+        }
+    }
+}
